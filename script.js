b5a82cd0ea816260c02f081639c459168043b21e
mbaeza611-stack <mbaeza611@gmail.com>
2026-08-03 14:24:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -193,6 +193,27 @@ window.addEventListener("scroll", () => {
 
 });
 
+/*=========================================
+    MOBILE MENU
+=========================================*/
 
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
 
 
